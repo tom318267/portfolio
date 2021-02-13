8c991f0cfc56a205eb7957ac12d46af89aa619b3
Thomas Rago <thomasrago@Thomass-iMac.fios-router.home>
2021-02-13 15:05:00 -0500

    adjust media query

--- src/components /Navbar/Navbar.js
+++ src/components /Navbar/Navbar.js
@@ -4,11 +4,11 @@ import "./Navbar.scss";
 const Navbar = () => {
   return (
     <div className="Navbar">
-      <nav class="navbar navbar-expand-lg navbar-light bg-transparent">
+      <nav className="navbar navbar-expand-lg navbar-light bg-transparent">
         <h4>Tom Rago</h4>
 
         <button
-          class="navbar-toggler"
+          className="navbar-toggler"
           type="button"
           data-toggle="collapse"
           data-target="#navbarNav"
@@ -16,43 +16,34 @@ const Navbar = () => {
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <span class="navbar-toggler-icon"></span>
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-              <a class="nav-link about hvr-underline-from-center" href="#about">
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <a
+                className="nav-link about hvr-underline-from-center"
+                href="#about"
+              >
                 About
               </a>
             </li>
-            <li class="nav-item">
+            <li className="nav-item">
               <a
-                class="nav-link my-work hvr-underline-from-center"
+                className="nav-link my-work hvr-underline-from-center"
                 href="#my-work"
               >
                 My Work
               </a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link hvr-underline-from-center" href="#contact">
+            <li className="nav-item">
+              <a className="nav-link hvr-underline-from-center" href="#contact">
                 Contact
               </a>
             </li>
           </ul>
         </div>
       </nav>
-      {/* <div className="name">Tom Rago</div>
-      <nav className="nav-container">
-        <a href="#about">
-          <h4 className="hvr-underline-from-center">About Me</h4>
-        </a>
-        <a href="#my-work">
-          <h4 className="hvr-underline-from-center">My Work</h4>
-        </a>
-        <a href="#contact">
-          <h4 className="hvr-underline-from-center">Contact</h4>
-        </a>
-      </nav> */}
     </div>
   );
 };
